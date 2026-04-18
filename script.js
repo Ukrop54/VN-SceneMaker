76f4e989b632517fc4f399b3a313daba5936d3c1
Ukrop54 <yaroslavkhor55@gmail.com>
2026-04-18 16:30:48 +0200

    global showdown func

--- script.js
+++ script.js
@@ -11,16 +11,21 @@
 //    $("#show-sprstyle").toggleClass("bi-chevron-down bi-chevron-up");
 // }
 
-function toggleEditorMenu(id) {
-   $(`#${id}-editor_content`).slideToggle("slow", function () {});
-   $(`#${id}_arrow`).toggleClass("bi-chevron-down bi-chevron-up");
+// function toggleEditorMenu(id) {
+//    $(`#${id}-editor_content`).slideToggle("slow", function () {});
+//    $(`#${id}_arrow`).toggleClass("bi-chevron-down bi-chevron-up");
+// }
+
+function toggleMenu(id) {
+   $(`#${id}-content`).slideToggle("slow", function () {});
+   $(`#${id}-arrow`).toggleClass("bi-chevron-down bi-chevron-up");
 }
 
-$("#sprstyle-select-header").on("click", function () {
-   $("#sprstyle-content").slideToggle("slow", function () {});
-   // $("#sprstylemaindiv").removeClass("sprstylemaindiv1");
-   $("#show-sprstyle").toggleClass("bi-chevron-down bi-chevron-up");
-});
+// $("#sprstyle-select-header").on("click", function () {
+//    $("#sprstyle-content").slideToggle("slow", function () {});
+//    // $("#sprstylemaindiv").removeClass("sprstylemaindiv1");
+//    $("#show-sprstyle").toggleClass("bi-chevron-down bi-chevron-up");
+// });
 
 function workinprogress() {
    alert("This feature is still in development. Please check back later.");
